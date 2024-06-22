d84279ce1c403f437ba1b6d9dca819ce02a422b0
gornakp <53863654+nakedsp@users.noreply.github.com>
2024-06-22 17:48:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -70,8 +70,8 @@ const BLOG = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
-    '"Noto Sans SC"',
     '-apple-system',
+    '"Noto Sans SC"',
     '"PingFang SC"',
     'HarmonyOS_Regular',
     'BlinkMacSystemFont',
